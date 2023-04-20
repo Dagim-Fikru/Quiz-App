b126d1b4c160430dc1a4d5d0f89bffad8ccc5602
Dagim Fikru <dagimfikru@gmail.com>
2023-04-21 01:54:01 +0300

    Formating the documents

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const questionArray = [
-  {
-    question: "What is the capital of France?",
-    answer: "Paris",
-    options: ["Paris", "London", "Berlin", "Rome"],
-  },
-  {
-    question: "What is the capital of Germany?",
-    answer: "Berlin",
-    options: ["Paris", "London", "Berlin", "Rome"],
-  },
-  {
-    question: "What is the capital of Italy?",
-    answer: "Rome",
-    options: ["Paris", "London", "Berlin", "Rome"],
-  },
-  {
-    question: "What is the capital of England?",
-    answer: "London",
-    options: ["Paris", "London", "Berlin", "Rome"],
-  },
-  {
-    question: "What is the capital of Spain?",
-    answer: "Madrid",
-    options: ["Paris", "London", "Berlin", "Madrid"],
-  },
+    {
+        question: "What is the capital of Ethiopia?",
+        answer: "Addis Ababa",
+        options: ["Addis Ababa", "London", "Berlin", "Rome"],
+    },
+    {
+        question: "What is the capital of Germany?",
+        answer: "Berlin",
+        options: ["London", "Addis Ababa", "Berlin", "Rome"],
+    },
+    {
+        question: "What is the capital of Italy?",
+        answer: "Rome",
+        options: ["London", "Addis Ababa", "Berlin", "Paris"],
+    },
+    {
+        question: "What is the capital of England?",
+        answer: "London",
+        options: ["Paris", "Addis Ababa", "Berlin", "London"],
+    },
+    {
+        question: "What is the capital of Spain?",
+        answer: "Madrid",
+        options: ["London", "Addis Ababa", "Madrid", "Rome"],
+    },
 ];
 // console.log(questionArray[0].options[0]);
 
@@ -34,65 +34,65 @@ let currentQuestionIndex = 0;
 let score = 0;
 
 function startQuiz() {
-  currentQuestionIndex = 0;
-  score = 0;
-  next.innerHTML = "Next";
-  showQuestion();
+    currentQuestionIndex = 0;
+    score = 0;
+    next.innerHTML = "Next";
+    showQuestion();
 }
 function showQuestion() {
-  let currentQuestion = questionArray[currentQuestionIndex];
-  let questionNumber = currentQuestionIndex + 1;
-  questions.innerHTML =
-    "Question " + questionNumber + ": " + currentQuestion.question;
-  choices.innerHTML = "";
-  currentQuestion.options.forEach(function (option) {
-    const button = document.createElement("button");
-    button.innerHTML = option;
-    button.classList.add("btn");
-    choices.appendChild(button);
-    if (option == currentQuestion.answer) {
-      button.dataset.correct = true;
-    }
+    let currentQuestion = questionArray[currentQuestionIndex];
+    let questionNumber = currentQuestionIndex + 1;
+    questions.innerHTML =
+        "Question " + questionNumber + ": " + currentQuestion.question;
+    choices.innerHTML = "";
+    currentQuestion.options.forEach(function (option) {
+        const button = document.createElement("button");
+        button.innerHTML = option;
+        button.classList.add("btn");
+        choices.appendChild(button);
+        if (option == currentQuestion.answer) {
+            button.dataset.correct = true;
+        }
 
-    button.addEventListener("click", checkAnswer);
-  });
+        button.addEventListener("click", checkAnswer);
+    });
 }
 // console.log(questionArray[0].options[0]);
 function checkAnswer(e) {
-  const selectedButton = e.target;
-  const correct = selectedButton.dataset.correct;
-  Array.from(choices.children).forEach(function (button) {
-    if (button.dataset.correct) {
-      button.classList.add("correct");
-    } else {
-      button.classList.add("wrong");
-    }
-  });
-  next.style.display = "block";
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    Array.from(choices.children).forEach(function (button) {
+        if (button.dataset.correct) {
+            button.classList.add("correct");
+        } else {
+            button.classList.add("wrong");
+        }
+    });
+    next.style.display = "block";
 
-  if (correct) {
-    score++;
-  }
-  next.addEventListener("click", nextQuestion);
+    if (correct) {
+        score++;
+    }
+    next.addEventListener("click", nextQuestion);
 }
 function nextQuestion() {
-  if (currentQuestionIndex < questionArray.length) {
-    currentQuestionIndex++;
     if (currentQuestionIndex < questionArray.length) {
-      showQuestion();
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questionArray.length) {
+            showQuestion();
+        } else {
+            next.style.display = "none";
+            while (choices.firstChild) {
+                choices.removeChild(choices.firstChild);
+            }
+            question.innerHTML =
+                "You got " + score + " out of " + questionArray.length + " correct!";
+            next.innerHTML = "Restart";
+            next.style.display = "block";
+        }
     } else {
-      next.style.display = "none";
-      while (choices.firstChild) {
-        choices.removeChild(choices.firstChild);
-      }
-      question.innerHTML =
-        "You got " + score + " out of " + questionArray.length + " correct!";
-      next.innerHTML = "Restart";
-      next.style.display = "block";
+        startQuiz();
     }
-  } else {
-    startQuiz();
-  }
 }
 
 startQuiz();
